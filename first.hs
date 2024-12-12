0ba6a399a1f3c7ae642cf90ec70546ee6e4fcf5f
Sarah <sarifishh@yahoo.com>
2024-12-12 12:12:24 +0000

    Update first.hs

--- first.hs
+++ first.hs
@@ -6,3 +6,11 @@ fibonacci n = fibonacci (n-1) + fibonacci (n-2)
 mySum :: Num a => [a] -> a
 mySum [] = 0
 mySum (x:xs) = x + mySum(xs)
+
+
+product [] = 1
+product (x:xs) = x * product(xs)
+
+length :: [a] 
+length [] = 0
+length (_:xs) = 1 + length(xs) 
